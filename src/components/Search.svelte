36635a3c181282e322b8baa0c6c3e8b9580c03f9
Diego Franco <diegofhrg@gmail.com>
2020-04-21 18:01:57 -0500

    Add search style

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -28,15 +28,25 @@
 </script>
 
 <style>
-    dt-search-container-style {
-        border-bottom: 1px solid lightgray;
-        height: 30px;
-        padding: 0px;
-        color: darkgrey;
+    .dt-search-container-style {
         width: 100%;
+        max-width: 350px;
     }
+    .dt-search-input-style{
+        background: none;
+        color: lightgray;
+        font-size: 18px;
+        padding: 10px 10px 10px 5px;
+        display: block;
+        border: none;
+        border-radius: 0;
+        border-bottom: 1px solid lightgray;
+    }
+
 </style>
 
 <div class="dt-search-container-layout dt-search-container-style">
-    <input id = "dtInput" type="text" placeholder={'Search...'} bind:value={searchKey}/>
+    <input id = "dtInput"
+           class="dt-search-input-style"
+           type="text" placeholder={'Search...'} bind:value={searchKey}/>
 </div>
