7e4ae69755b77b2cd1c621f1126f09080993fff2
diego <diegofhrg@gmail.com>
2020-05-07 17:31:01 -0500

    Fix[sticky]: sticky column on refresh

--- src/App.svelte
+++ src/App.svelte
@@ -4,27 +4,27 @@
   import CustomHeader1 from './CustomHeader1.svelte';
   const minWidth = 300
 
-  const columns = [
+  let columns = [
     {
       label: 'Id',
       field: 'id',
       numeric: true,
       sortable: true,
       sticky: true,
-      minWidth
+      minWidth,
     },
     {
       label: 'Name',
       field: 'name',
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       label: 'fav provider',
       field: 'provider',
       component: CustomCol1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       label: 'fav social network',
@@ -36,77 +36,154 @@
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
     {
       field: 'socialNetwork',
       component: CustomCol1,
       headerComponent: CustomHeader1,
       sortable: true,
-      minWidth
+      minWidth,
     },
   ];
   let rows = [
-    { name: 'Diego', id: 0, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'a', id: 1, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'b', id: 2, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'c', id: 3, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'd', id: 4, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'e', id: 5, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'f', id: 6, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'g', id: 7, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'h', id: 8, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'i', id: 9, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'j', id: 10, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'k', id: 11, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'l', id: 12, provider: 'github.com', socialNetwork: 'facebook.com' },
-    { name: 'Juan Pablo', id: 13, provider: 'gitlab.com', socialNetwork: 'google+.com' },
-    { name: 'Juan Pablo', id: 14, provider: 'gitlab.com', socialNetwork: 'google+.com' },
+    { name: 'AA', lastName: 'Franco', provider: 'github.com', socialNetwork: 'facebook.com' },
+    { name: 'a', lastName: '1', provider: 'github.com', socialNetwork: 'facebook.com' },
   ];
   const fuseConfig = {
     keys: ['id', 'name', 'provider', 'socialNetwork']
   }
-  // setTimeout(()=> {
-  //  rows = [
-  //    { name: 'AA', lastName: 'Franco', provider: 'github.com', socialNetwork: 'facebook.com' },
-  //    { name: 'a', lastName: '1', provider: 'github.com', socialNetwork: 'facebook.com' },
-  //  ]
-  // }, 1500)
+  setTimeout(()=> {
+    columns = [
+      {
+        label: 'Id',
+        field: 'id',
+        numeric: true,
+        sortable: true,
+        sticky: true,
+        minWidth,
+      },
+      {
+        label: 'Name',
+        field: 'name',
+        sortable: true,
+        minWidth,
+      },
+      {
+        label: 'fav provider',
+        field: 'provider',
+        component: CustomCol1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        label: 'fav social network',
+        field: 'socialNetwork',
+        component: CustomCol1,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+      {
+        field: 'socialNetwork',
+        component: CustomCol1,
+        headerComponent: CustomHeader1,
+        sortable: true,
+        minWidth,
+      },
+    ];
+    rows = [
+      { name: 'Diego', id: 0, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'a', id: 1, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'b', id: 2, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'c', id: 3, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'd', id: 4, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'e', id: 5, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'f', id: 6, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'g', id: 7, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'h', id: 8, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'i', id: 9, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'j', id: 10, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'k', id: 11, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'l', id: 12, provider: 'github.com', socialNetwork: 'facebook.com' },
+      { name: 'Juan Pablo', id: 13, provider: 'gitlab.com', socialNetwork: 'google+.com' },
+      { name: 'Juan Pablo', id: 14, provider: 'gitlab.com', socialNetwork: 'google+.com' },
+    ];
+  }, 3000)
   function headerClickHandler(event){
     console.log(event)
   }
